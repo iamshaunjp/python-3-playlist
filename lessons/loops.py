0355b6310a08a2bab58a7a9d321ffee7e1150483
iamshaunjp <shaunjp@shaun-pelling.com>
2017-06-01 17:26:10 +0100

    added lesson 10 code

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,20 +1,36 @@
-# FOR LOOPS
-# for loops can iterate over data within lists
-ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+# # FOR LOOPS
+# # for loops can iterate over data within lists
+# ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+#
+# for ninja in ninjas:
+#     print(ninja)
+#
+# # can give for loops a range using slicing
+# print('-----')
+# for ninja in ninjas[1:3]:
+#     print(ninja)
+#
+# # add additional logic to for loops
+# print('-----')
+# for ninja in ninjas:
+#     if ninja == 'yoshi':
+#         print(f'{ninja} - black belt')
+#         break
+#     else:
+#         print(ninja)
 
-for ninja in ninjas:
-    print(ninja)
+# WHILE LOOP STATEMENT
+age = 25
+num = 0
 
-# can give for loops a range using slicing
-print('-----')
-for ninja in ninjas[1:3]:
-    print(ninja)
-
-# add additional logic to for loops
-print('-----')
-for ninja in ninjas:
-    if ninja == 'yoshi':
-        print(f'{ninja} - black belt')
+while num < age:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print(num)
+    num += 1
+    if num > 10:
         break
-    else:
-        print(ninja)
+else:
+    print('outside of condition') #doesn't work when using break

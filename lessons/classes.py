ef854d6fc84695727a070b4bf133140bdd887144
iamshaunjp <shaunjp@shaun-pelling.com>
2017-06-02 11:35:19 +0100

    added lesson 17 code

--- lessons/classes.py
+++ lessons/classes.py
@@ -1,16 +1,22 @@
 class Planet:
 
-    def __init__(self):
-        self.name = 'Hoth'
-        self.radius = 200000
-        self.gravity = 5.5
-        self.system = 'Hoth System'
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system}'
 
-hoth = Planet()
+hoth = Planet('Hoth', 200000, 5.5, 'Hoth System')
 print('Planet stats:')
 print(f'Name: {hoth.name}')
 print(f'System: {hoth.system}')
 print(hoth.orbit())
+
+naboo= Planet('Naboo', 300000, 8, 'Naboo System')
+print('Planet stats:')
+print(f'Name: {naboo.name}')
+print(f'System: {naboo.system}')
+print(naboo.orbit())

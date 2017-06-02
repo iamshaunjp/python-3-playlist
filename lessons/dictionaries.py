b2f75cded636bc7779f868eeea333f57d430425d
iamshaunjp <shaunjp@shaun-pelling.com>
2017-06-02 10:11:52 +0100

    added lesson 15 code

--- lessons/dictionaries.py
+++ lessons/dictionaries.py
@@ -1,6 +1,12 @@
-def ninja_intro(dict):
-    for key, val in dict.items():
-        print(f'I am {key} and I am a {val} belt')
+def belt_count(dict):
+    belts = list(dict.values())
+    for belt in set(belts):
+        num = belts.count(belt)
+        print(f'There are {num} {belt} belts')
+
+# def ninja_intro(dict):
+#     for key, val in dict.items():
+#         print(f'I am {key} and I am a {val} belt')
 
 ninja_belts = { }
 
@@ -15,4 +21,5 @@ while True:
     else:
         break
 
-ninja_intro(ninja_belts)
+# ninja_intro(ninja_belts)
+belt_count(ninja_belts)
